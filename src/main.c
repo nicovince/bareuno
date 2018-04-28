215a4aac5247c785df28a0f53783551b4f06df5c
Nicolas VINCENT <nico.vince@gmail.com>
2018-04-28 15:54:35 +0200

    Sends data in slip frame

--- src/main.c
+++ src/main.c
@@ -2,6 +2,10 @@
 #include "board.h"
 #include <avr/interrupt.h>
 #include "usart.h"
+#include "slip.h"
+slip_decoder_t slip;
+uint8_t slip_buffer[128];
+uint8_t slip_encoded[256];
 
 int main(void)
 {
@@ -10,18 +14,22 @@ int main(void)
     BOARD_PIN13_PORT &= ~_BV(BOARD_PIN13_PIN);
 
     setup_usart(BAUD_9600);
+    init_slip_decoder(&slip, slip_buffer, 128);
 
     //uint8_t buf[] = {'0', '\r', '\n'};
+    uint8_t c;
     while(1)
     {
-        uint8_t c;
+        int16_t s = slip_encode(&c, slip_encoded, 1);
+        usart_write(slip_encoded, s);
+        ++c;
+
         size_t sz = usart_read(&c, 1);
         if (sz == 0)
             continue;
         if (c == 'a') {
             sbi(BOARD_PIN13_TOGGLE_REG, BOARD_PIN13_TOGGLE_BIT);
         }
-        usart_write(&c, 1);
     }
     return 0;
 }
