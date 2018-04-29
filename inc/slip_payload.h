8a55e6d406bd47ca477f95960770a56b3f971388
Nicolas VINCENT <nico.vince@gmail.com>
2018-04-29 23:10:15 +0200

    send pid function

--- inc/slip_payload.h
+++ inc/slip_payload.h
@@ -9,6 +9,7 @@
 #define LEN_OFFSET 2
 #define DATA_OFFSET 3
 #define CRC_SIZE 2
+#pragma pack(push, 1)
 typedef struct {
     uint8_t pid; // Primitive id
     uint8_t seq; // sequence number
@@ -16,6 +17,7 @@ typedef struct {
     uint8_t data[MAX_SLIP_PAYLOAD-DATA_OFFSET-CRC_SIZE];
     uint16_t crc; // CRC16-CCITT
 } slip_payload_t;
+#pragma pack(pop)
 
 uint16_t pack_slip_payload(slip_payload_t *slip_payload, uint8_t *raw_slip_payload);
 int8_t unpack_slip_payload(uint8_t *raw_slip_payload, slip_payload_t *slip_payload);
--- src/main.c
+++ src/main.c
@@ -10,6 +10,18 @@ slip_decoder_t slip;
 uint8_t slip_buffer[128];
 uint8_t slip_encoded[256];
 uint8_t raw_slip_payload[MAX_SLIP_PAYLOAD];
+slip_payload_t slip_payload;
+
+void send_pid(uint8_t pid)
+{
+    static uint8_t cnt = 0;
+    slip_payload.pid = pid;
+    slip_payload.seq = cnt++;
+    slip_payload.len = 0;
+    uint16_t slip_payload_size = pack_slip_payload(&slip_payload, raw_slip_payload);
+    uint8_t tx_slip_size = slip_encode(raw_slip_payload, slip_encoded, slip_payload_size);
+    usart_write(slip_encoded, tx_slip_size);
+}
 
 int main(void)
 {
@@ -18,35 +30,40 @@ int main(void)
     BOARD_PIN13_PORT &= ~_BV(BOARD_PIN13_PIN);
     int16_t slip_size;
 
-    setup_usart(BAUD_9600);
+    setup_usart(BAUD_57600);
     init_slip_decoder(&slip, slip_buffer, 128);
 
     uint8_t c;
+    //while(1)
+    //{
+    //    send_pid(0x1);
+    //}
     while(1)
     {
+
         size_t sz = usart_read(&c, 1);
         if (sz != 0) {
             slip_size = slip_decode(&slip, c);
             if (slip_size == 0) {
                 /* Reset empty message */
                 reset_slip_decoder(&slip);
-            }
-        }
-        if ((sz != 0) && (slip_size > 0))
-        {
-            slip_payload_t slip_payload;
-            int8_t crc_ok = unpack_slip_payload(slip.buf, &slip_payload);
-            /* TODO Check length of payload data is lesser than slip message length */
-            if (!crc_ok) {
-                board_pin_set(13);
-            } else if (slip_payload.pid == 6) {
-                board_pin_clear(13);
-            }
-            reset_slip_decoder(&slip);
-            uint16_t slip_payload_size = pack_slip_payload(&slip_payload, raw_slip_payload);
-            uint8_t tx_slip_size = slip_encode(raw_slip_payload, slip_encoded, slip_payload_size);
-            usart_write(slip_encoded, tx_slip_size);
+            } else if (slip_size > 0) {
+                int8_t crc_ok = unpack_slip_payload(slip.buf, &slip_payload);
+                /* TODO Check length of payload data is lesser than slip message length */
+                if (!crc_ok) {
+                    //board_pin_set(13);
+                    send_pid(slip_size);
+                } else if (slip_payload.pid == 6) {
+                    //board_pin_toggle(13);
+                    //sbi(BOARD_PIN13_TOGGLE_REG, BOARD_PIN13_TOGGLE_BIT);
+                    send_pid(0xFF);
+                }
+                reset_slip_decoder(&slip);
+                //uint16_t slip_payload_size = pack_slip_payload(&slip_payload, raw_slip_payload);
+                //uint8_t tx_slip_size = slip_encode(raw_slip_payload, slip_encoded, slip_payload_size);
+                //usart_write(slip_encoded, tx_slip_size);
 
+            }
         }
     }
     return 0;
