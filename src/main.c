29e7e292ede71adc842a47d8cecc0f57564b744f
Nicolas VINCENT <nico.vince@gmail.com>
2018-04-29 09:48:37 +0200

    Send and receive slip payload

--- src/main.c
+++ src/main.c
@@ -3,33 +3,55 @@
 #include <avr/interrupt.h>
 #include "usart.h"
 #include "slip.h"
+#include "slip_payload.h"
 slip_decoder_t slip;
 uint8_t slip_buffer[128];
 uint8_t slip_encoded[256];
+uint8_t raw_slip_payload[MAX_SLIP_PAYLOAD];
 
 int main(void)
 {
     sei();
     BOARD_PIN13_DIR_REG |= _BV(BOARD_PIN13_DIR_BIT);
     BOARD_PIN13_PORT &= ~_BV(BOARD_PIN13_PIN);
+    int16_t slip_size;
 
     setup_usart(BAUD_9600);
     init_slip_decoder(&slip, slip_buffer, 128);
 
-    //uint8_t buf[] = {'0', '\r', '\n'};
     uint8_t c;
     while(1)
     {
-        int16_t s = slip_encode(&c, slip_encoded, 1);
-        usart_write(slip_encoded, s);
-        ++c;
-
         size_t sz = usart_read(&c, 1);
+        if (sz != 0) {
+            slip_size = slip_decode(&slip, c);
+            if (slip_size == 0) {
+                /* Reset empty message */
+                reset_slip_decoder(&slip);
+            }
+        };
+        if ((sz != 0) && (slip_size > 0))
+        {
+            slip_payload_t slip_payload;
+            unpack_slip_payload(slip.buf, &slip_payload);
+            /* TODO Check length of payload data is lesser than slip message length */
+            if (slip_payload.pid == 6)
+            {
+                sbi(BOARD_PIN13_TOGGLE_REG, BOARD_PIN13_TOGGLE_BIT);
+            }
+            reset_slip_decoder(&slip);
+            uint16_t slip_payload_size = pack_slip_payload(&slip_payload, raw_slip_payload);
+            uint8_t tx_slip_size = slip_encode(raw_slip_payload, slip_encoded, slip_payload_size);
+            usart_write(slip_encoded, tx_slip_size);
+
+        }
+#if 0
         if (sz == 0)
             continue;
         if (c == 'a') {
             sbi(BOARD_PIN13_TOGGLE_REG, BOARD_PIN13_TOGGLE_BIT);
         }
+#endif
     }
     return 0;
 }
--- test/slip_test.c
+++ test/slip_test.c
@@ -2,30 +2,32 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 #include "slip.h"
 #include "slip_payload.h"
 
 void print_slip_payload(slip_payload_t * slip_payload)
 {
-    printf("pid: %d\n", slip_payload->pid);
-    printf("seq: %d\n", slip_payload->seq);
-    printf("len: %d\n", slip_payload->len);
+    printf("pid: %02X\n", slip_payload->pid);
+    printf("seq: %02X\n", slip_payload->seq);
+    printf("len: %02X\n", slip_payload->len);
     for (uint8_t i = 0; i < slip_payload->len; ++i)
     {
         printf("data[%d] %02X\n", i, slip_payload->data[i]);
     }
     printf("crc: %02X\n", slip_payload->crc);
 }
-    
+
 
 bool check_unpack(slip_payload_t* ref, uint8_t *raw_slip_payload)
 {
     slip_payload_t slip_unpacked;
     unpack_slip_payload(raw_slip_payload, &slip_unpacked);
-    print_slip_payload(ref);
-    print_slip_payload(&slip_unpacked);
-    
+
     bool res = true;
     res &= (ref->pid == slip_unpacked.pid);
     res &= (ref->seq == slip_unpacked.seq);
@@ -44,19 +46,37 @@ int main(int argc, char** argv)
     slip_payload_t slip_payload;
     memset(raw_slip_payload, 0 , MAX_SLIP_PAYLOAD);
     srand(pid);
-    
+
     slip_payload.pid = pid;
     slip_payload.seq = (uint8_t) rand();
     slip_payload.len = 7;
-    
+
     printf("%s : %d\n", arduino, pid);
     uint16_t slip_payload_size = pack_slip_payload(&slip_payload, raw_slip_payload);
-    
+
     bool res = check_unpack(&slip_payload, raw_slip_payload);
     printf("check_unpack: %d\n", res);
+    print_slip_payload(&slip_payload);
 
+    /* Send Slip buffer */
     uint8_t slip_size = slip_encode(raw_slip_payload, slip_buffer, slip_payload_size);
-    
-
+    printf("Sending: ");
+    for (int i = 0; i < slip_size; ++i)
+    {
+        printf("%02X", slip_buffer[i]);
+    }
+    printf("\n");
+    int fd = open(arduino, O_RDWR);
+    ssize_t wr_sz = write(fd, slip_buffer, slip_size);
+    if (wr_sz != slip_size)
+    {
+        printf("Written %d bytes out of %d\n", (int)wr_sz, (int)slip_size);
+    }
+    while(1)
+    {
+        uint8_t b;
+        read(fd, &b, 1);
+        printf("%02X\n", b);
+    }
     return 0;
 }
