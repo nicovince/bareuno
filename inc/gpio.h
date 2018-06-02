710e9a89d91bb9df9a8c0e2f904fbf830510277d
Nicolas VINCENT <nico.vince@gmail.com>
2018-06-02 19:18:33 +0200

    Set debug define in gpio.h
    pins where leds are connected

--- inc/gpio.h
+++ inc/gpio.h
@@ -1,6 +1,8 @@
 #ifndef __GPIO_H__
 #define __GPIO_H__
 #include <stdint.h>
+#define GREEN_LED 5
+#define YELLOW_LED 7
 
 uint8_t board_pin_to_port(uint8_t board_pin);
 uint8_t board_pin_to_port_pin(uint8_t board_pin);
